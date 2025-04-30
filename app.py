eb709288c7134928bfff9707f311ed2a6d639d01
Weslley Costa <weslleydacostasebastiao@gmail.com>
2025-04-29 21:52:39 -0300

    Start completo

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 # Carrega as variáveis de ambiente do arquivo .env
 load_dotenv()
 
+
 # Importa as classes e funções necessárias do LangChain e dotenv
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
@@ -12,7 +13,7 @@ from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_community.chat_message_histories import ChatMessageHistory
 
 # Este texto define o comportamento do assistente e como ele deve interagir com o usuário.
-teamplate = """Você é um assistente pessoal que ajuda o usuário a gerir sua agenda de horarios, lembretes e afazeres diarios.
+template = """Você é um assistente pessoal que ajuda o usuário a gerir sua agenda de horarios, lembretes e afazeres diarios.
 Você faz isso a partir do acesso atraves de tools a sua agenda.
 
 Historico de conversa:
@@ -24,12 +25,12 @@ Entrada do usuário:
 
 # Cria um modelo de prompt a partir de uma lista de mensagens
 prompt = ChatPromptTemplate.from_messages([
-    ("system", teamplate), # ("system", teamplate): Define o contexto inicial do assistente.
+    ("system", template), # ("system", teamplate): Define o contexto inicial do assistente.
     MessagesPlaceholder(variable_name="history"), # MessagesPlaceholder: Espaço reservado para o histórico de mensagens da conversa.
     ("human", "{input}") # ("human", "{input}"): Representa a entrada do usuário.
 ])
 
-llm = ChatOpenAI(temperature=0, model="á definir")
+llm = ChatOpenAI(temperature=0.7, model="gpt-4o-mini")
 
 chain = prompt | llm # Encadeia o modelo de prompt com o modelo de linguagem (LLM) para criar uma cadeia de execuçãoso.
 
@@ -40,3 +41,31 @@ def get_session_history(session_id: str) -> BaseChatMessageHistory:
     if session_id not in store:
         store[session_id] = ChatMessageHistory()
     return store[session_id]
+
+chain_with_history = RunnableWithMessageHistory(
+    chain,
+    get_session_history,
+    input_messages_key="input",
+    history_messages_key="history"
+)
+
+
+def iniciar_agente():
+    print("Bem-vindo ao assistente de agenda!")
+    while True:
+        pergunta_usuario = input("Você: ")
+
+        if pergunta_usuario.lower() in ["sair", "exit"]:
+            print("Desligando...")
+            break
+
+        resposta = chain_with_history.invoke(
+            {'input': pergunta_usuario},
+            config= {'configurable': {'session_id': 'user123'}}
+        )
+
+        print("Assistente: ", resposta.content)
+
+
+if __name__ == "__main__":
+    iniciar_agente()
